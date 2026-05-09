88a9f4a61b2c1f7a52a12a2770373a61c705a883
Your Name <ntokosnas@gmail.com>
2026-05-09 19:58:56 +0300

    Implemented ICD10 bulk import

---
+++ load.sql
@@ -0,0 +1,37 @@
+--load.sql
+--Data population script for the database Hospital_DB
+
+--Database's Name 
+USE mydb;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ========================================
+--1.Reference tables
+-- ========================================
+
+-- ========================================
+-- ICD10 IMPORT
+-- ========================================
+
+LOAD DATA LOCAL INFILE 'C:/Users/ntoko/Hospital_DB/csv/icd10_utf8.txt'
+INTO TABLE `mydb`.`ICD-10`
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY '\t'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`ICD-10`, `Description`);
+
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+-- KEN
+-- Insurance providers 
+-- Medicines 
+-- Active substances
+-- DRUG
+-- DRUG_has_Active_Substance
+
+
